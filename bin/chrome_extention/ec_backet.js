00ec9b522cb8aac5f0f1505c412d88bbce1a0f36
hoge fuga <hoge@fuga>
2017-04-24 07:17:43 +0000

    chrome extension

--- bin/chrome_extention/ec_backet.js
+++ bin/chrome_extention/ec_backet.js
@@ -2,22 +2,36 @@
   chrome.browserAction.onClicked.addListener(function(tab) { 
     chrome.tabs.getSelected(null, function(tab) {  
       var url = tab.url;
-      if ( url == ""){
-        
-        
-        var options = {user_id: 1};
+      if ( url.indexOf('ec-basket.herokuapp.com/users/') != -1 ){
+        var urls = url.split("/");
+        var user_id = urls[urls.length-1]; 
+        alert("User ID : " + user_id + " Registered!!");
         // データ保存
-        chrome.storage.sync.set(options, function(){});
+        chrome.storage.local.set({user_id: user_id, env: "p"}, function(){});
+        
+      }else if( url.indexOf('https://techacademy-sunaga.c9users.io/users/') != -1){
+        var urls = url.split("/");
+        var user_id = urls[urls.length-1]; 
+        alert("User ID : " + user_id + " Registered!!");
+        // データ保存
+        chrome.storage.local.set({user_id: user_id, env: "d"}, function(){});
+        
       }else{
         // データ取得
-        chrome.storage.sync.get(
-          defaults,
-          function(items) {
-            var user_id = items.user_id;
+        chrome.storage.local.get(
+          ["user_id", "env"],
+          function(vals) {
+            var user_id = vals.user_id;
+            var env = vals.env;
+            alert("商品を追加しました！");
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", "https://techacademy-sunaga.c9users.io/api/item_create", true);
+            if(env == "p"){
+              xhr.open("POST", "https://ec-basket.herokuapp.com/api/item_create", true);
+            }else{
+              xhr.open("POST", "https://techacademy-sunaga.c9users.io/api/item_create", true);
+            }
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send("id="+user_id+"&url=" + url);
+            xhr.send("id="+user_id+"&url=" + encodeURIComponent(url));
         });
       }
     });  
